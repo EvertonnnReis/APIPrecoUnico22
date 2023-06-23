5a42369d37f368e4e8dc702b435ea496086c47aa
Evertonnreis <evertonreisnascimento3@gmail.com>
2023-06-23 11:32:15 -0300

    Primeiro commit

---
+++ APIPrecoUnico22/Controllers/RegistroController.cs
@@ -0,0 +1,29 @@
+﻿using APIPrecoUnico22.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace APIPrecoUnico22.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RegistroController : ControllerBase
+    {
+        private readonly IServices _services;
+
+        public RegistroController(IConfiguration configuration,
+            IServices services)
+        {
+            _services = services;
+        }
+
+
+
+        [HttpGet]
+        public async Task<dynamic> ObterRegistros()
+        {
+            var registro = await _services.ObterRegistros("87474770-319C-4180-8ADF-39D59A50763F");
+
+            // Retorna os registros como resposta JSON
+            return new { registro };
+        }
+    }
+}
---
+++ APIPrecoUnico22/Services/IServices.cs
@@ -0,0 +1,11 @@
+﻿using APIPrecoUnico22.Models;
+
+namespace APIPrecoUnico22.Services
+{
+    public interface IServices
+    {
+        Task<dynamic> ObterRegistros(string _interface);
+        Task EnviarApiSDP(string caminhoArquivo, List<string> listaSku);
+        Task SalvarRegistrosEmArquivo(List<RegistroModel> registros, string caminhoArquivo);
+    }
+}
